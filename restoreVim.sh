797a95f0a65772c0afea57f8d2b984b05f79fdd0
Gerardo Reyes <grevych@gmail.com>
2018-01-30 02:43:06 -0600

    Update restore script:  - Add powerline fonts install  - Clean up bundles installation

--- restoreVim.sh
+++ restoreVim.sh
@@ -4,6 +4,7 @@ HOMEDIR=$(cd $(dirname "$0"); pwd)
 VIM_HOME=~/.vim
 VIM_COLORS=$VIM_HOME/colors
 VIM_BUNDLE=$VIM_HOME/bundle
+VIM_FONTS=$VIM_HOME/fonts
 
 mkdir -p $VIM_HOME
 mkdir -p $VIM_COLORS
@@ -11,18 +12,23 @@ mkdir -p $VIM_BUNDLE
 
 
 # Install Vundle
+rm -rf $VIM_BUNDLE/Vundle.vim
 git clone https://github.com/VundleVim/Vundle.vim.git $VIM_BUNDLE/Vundle.vim
 
-# Remove previous vimrc file
-rm -f ~/.vimrc
-ln -s $HOMEDIR/vundlerc ~/.vimrc
-
-#Download plugins
-vim +PluginInstall +qall
+# Install Fonts
+rm -rf $VIM_FONTS/repo
+git clone https://github.com/powerline/fonts.git $VIM_FONTS/repo --depth=1
+$VIM_FONTS/repo/install.sh
 
 # Copy colors from bundles
 cp $VIM_BUNDLE/tender/colors/* $VIM_COLORS/
 
+# Remove previous vimrc file
+rm -f ~/.vimrc
+
 # Restore vimrc file
 ln -s $HOMEDIR/vimrc ~/.vimrc
+
+#Download plugins
+vim +PluginInstall +qall
 
